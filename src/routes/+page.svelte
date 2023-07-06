663ce6c290f98cf0a13e7365af80cd650429d2a4
FomasTreeman <tom@team-freeman.com>
2023-07-07 00:18:12 +0100

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,29 +7,39 @@
   $: console.log(data);
   let repos;
 
-  $: {
-    if (data.user) {
-      fetch(`https://api.github.com/users/${data.user.login}}/repos`, {
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: data.token,
-        },
-      })
-        .then((resp) => resp.json())
-        .then(
-          (json) =>
-            (repos = [
-              ...json.map((repo) => ({
-                name: repo.name,
-              })),
-            ])
-        );
-    }
-  }
+  // $: {
+  //   if (data.user) {
+  //     fetch(`https://api.github.com/users/${data.user.login}}/repos`, {
+  //       headers: {
+  //         Accept: 'application/vnd.github+json',
+  //         Authorization: data.token,
+  //       },
+  //     })
+  //       .then((resp) => resp.json())
+  //       .then(
+  //         (json) =>
+  //           (repos = [
+  //             ...json.map((repo) => ({
+  //               name: repo.name,
+  //             })),
+  //           ])
+  //       );
+  //   }
+  // }
 </script>
 
-<h1>Welcome {data.user}</h1>
+<main>
+  <h1>Welcome {data.user?.login}</h1>
+  <a href="/login">
+    <button>Login using Github</button>
+  </a>
+</main>
 
-<a href="/login">
-  <button>Login using Github</button>
-</a>
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+</style>
