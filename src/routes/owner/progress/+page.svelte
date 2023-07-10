4c0c6332fe6aee3f4f9f1178fecd63eec8dda2de
FomasTreeman <tom@team-freeman.com>
2023-07-10 20:57:54 +0100

    corrected and refactores formukla

--- src/routes/owner/progress/+page.svelte
+++ src/routes/owner/progress/+page.svelte
@@ -1,59 +1,31 @@
 <script lang="ts">
-  import { Page, Progressbar } from 'konsta/svelte';
+  import {
+    Page,
+    Progressbar,
+    Block,
+    Breadcrumbs,
+    BreadcrumbsItem,
+    BreadcrumbsSeparator,
+    List,
+    ListItem,
+  } from 'konsta/svelte';
   import type { IIssue } from '../../../types/types';
   export let data;
   let expectedProgress = 0;
   let actualProgress = 0;
 
-  /**
-   * Get the total expected or actuals from a repo
-   * @param {string} type - expected or actual (E or A)
-   * @return {number}
-   */
-  const expectedActualTotal = (type: string) => {
-    //why doesnt jsdoc work here?
-    return data.issues.reduce(
+  $: {
+    const estimated = data.issues.reduce(
       (acc: number, issue: any) =>
         acc +
           parseInt(
             issue.labels
-              .find((label: string) => label.toUpperCase().includes(type))
+              .find((label: string) => label.toUpperCase().includes('E'))
               ?.slice(1)
           ) || 0,
       0
     );
-  };
-
-  $: {
-    const totalExpected = expectedActualTotal('E');
-    // const totalActuals = expectedActualTotal('A');
-    const totalClosed: number = data.issues.reduce(
-      (acc: number, issue: IIssue): number => {
-        const actual = issue.labels.find((label) => label.includes('A'));
-        const expected = issue.labels.find((label) => label.includes('E'));
-        return issue.state === 'closed'
-          ? actual
-            ? acc + parseInt(actual[1])
-            : expected
-            ? acc + parseInt(expected[1])
-            : acc
-          : acc;
-      },
-      0
-    );
-    const totalClosedExpected = data.issues.reduce(
-      (acc: number, issue: IIssue): number => {
-        const actual = issue.labels.find((label) => label.includes('A'));
-        const expected = issue.labels.find((label) => label.includes('E'));
-        return issue.state === 'closed'
-          ? expected
-            ? acc + parseInt(expected[1])
-            : acc
-          : acc;
-      },
-      0
-    );
-    const totalExpectedActuals = data.issues.reduce(
+    const projected: number = data.issues.reduce(
       (acc: number, issue: IIssue): number => {
         const actual = issue.labels.find((label) => label.includes('A'));
         const expected = issue.labels.find((label) => label.includes('E'));
@@ -65,32 +37,54 @@
       },
       0
     );
+    const originalEstimate = data.issues.reduce(
+      (acc: number, issue: IIssue): number => {
+        const actual = issue.labels.find((label) => label.includes('A'));
+        const expected = issue.labels.find((label) => label.includes('E'));
+        return issue.state === 'closed'
+          ? actual
+            ? acc + parseInt(actual[1])
+            : acc
+          : acc;
+      },
+      0
+    );
 
-    const expectedTotal = (100 / totalExpected) * totalClosedExpected || 0;
-    const actualTotal = (100 / totalExpectedActuals) * totalClosed || 0;
+    console.log(estimated, originalEstimate, projected);
+    const expectedTotal = (100 / estimated) * originalEstimate || 0;
+    const actualTotal = (100 / projected) * originalEstimate || 0;
     actualProgress = parseInt(actualTotal.toFixed(4)) / 100;
     expectedProgress = parseInt(expectedTotal.toFixed(4)) / 100;
   }
 </script>
 
 <Page>
+  <Block strongIos outlineIos>
+    <Breadcrumbs>
+      <a href="/owner/repos">
+        <BreadcrumbsItem>fac27</BreadcrumbsItem>
+      </a>
+      <BreadcrumbsSeparator />
+      <BreadcrumbsItem active>this repo</BreadcrumbsItem>
+    </Breadcrumbs>
+  </Block>
   <h2 class="m-20 text-4xl">
     Expected Progress: {(expectedProgress * 100).toFixed(2)} %
   </h2>
-  <Progressbar progress={expectedProgress} class="bg-slate-200 m-20" />
+  <Progressbar progress={expectedProgress} class="bg-slate-200 m-15" />
   <h2 class="m-20 text-4xl">
     Actual Progress: {(actualProgress * 100).toFixed(2)} %
   </h2>
   <Progressbar
     progress={actualProgress}
-    class="bg-slate-200 text-sky-400 m-20"
+    class="bg-slate-200 text-sky-400 m-15"
   />
-  <ul class="pl-5 m-8">
+  <List strong outline class="pl-5 m-20 pb-3">
     {#each data.issues as issue}
-      <h2>
+      <ListItem>
         {issue.labels.join(' -> ')}
         {issue.labels.toString().includes('A') ? '✅' : '❌'} : {issue.title}
-      </h2>
+      </ListItem>
     {/each}
-  </ul>
+  </List>
 </Page>
--- src/routes/owner/repos/+page.svelte
+++ src/routes/owner/repos/+page.svelte
@@ -1,46 +1,73 @@
 <script>
-  import { Page, Button } from 'konsta/svelte';
+  import { Page, Button, Block, Navbar, Preloader } from 'konsta/svelte';
 
   export let data;
   let org = '';
+  let repo = '';
+  let loadingOrg = false;
   let orgRepos = [{ name: '', url: '' }];
 
   async function getOrgRepos() {
+    loadingOrg = true;
     let response = await fetch(`/api/org?org=${org}`);
     const json = await response.json();
     orgRepos = [...json.repos];
+    loadingOrg = false;
   }
+
+  $: console.log(repo);
 </script>
 
 <Page>
-  <input
-    id="org"
-    name="org"
-    type="search"
-    class="bg-slate-800 text-slate-100 rounded-md p-2 m-10"
-    bind:value={org}
+  <Navbar
+    title="PICK YOUR REPO TO VIEW YOUR PROGRESS"
+    className="top-0 sticky"
   />
-  <div on:click={getOrgRepos}>
-    <Button>Add org</Button>
-  </div>
-  <form method="GET" action="/owner/progress" class="m-5">
-    <label for="repo" class="text-xl m-5">Choose a repo:</label>
-    <select
-      id="repo"
-      name="repo"
-      class="bg-slate-800 text-slate-100 rounded-md p-2"
-    >
-      {#each data.repos as repo}
-        <option value={repo.name}> {repo.name} </option>
-      {/each}
-      {#if orgRepos.length}
-        {#each orgRepos as repo}
-          <option value={repo.name}> {repo.name} </option>
-        {/each}
+  <div class="flex flex-col">
+    <Block strong inset class="flex items-center gap-4">
+      <label for="org" class="text-xl">Add an org:</label>
+      <input
+        id="org"
+        name="org"
+        type="search"
+        class="bg-slate-800 text-slate-100 rounded-md p-2"
+        bind:value={org}
+      />
+      {#if loadingOrg}
+        <Preloader />
+      {:else}
+        <button on:click={getOrgRepos}>
+          <Button>Add org</Button>
+        </button>
       {/if}
-    </select>
-    <Button raised rounded large class="k-color-brand-green m-10" tonal>
-      Check progress
-    </Button>
-  </form>
+    </Block>
+    <Block strong inset>
+      <form
+        method="get"
+        action="/owner/progress"
+        class="flex items-center gap-4"
+      >
+        <label for="repo" class="text-xl">Choose a repo:</label>
+        <select
+          id="repo"
+          name="repo"
+          bind:value={repo}
+          class="bg-slate-800 text-slate-100 rounded-md p-2"
+          required
+        >
+          {#each data.repos as repo}
+            <option value={repo.name}> {repo.name} </option>
+          {/each}
+          {#if orgRepos.length}
+            {#each orgRepos as repo}
+              <option value={repo.name}> {repo.name} </option>
+            {/each}
+          {/if}
+        </select>
+        <Button raised rounded large class="k-color-brand-green w-40" tonal>
+          Check progress
+        </Button>
+      </form>
+    </Block>
+  </div>
 </Page>
